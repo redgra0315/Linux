f65a1c9c321dc6e529764a5e7344c7e80bcc658b
redgra0315 <redgra0315@163.com>
2019-04-10 09:40:24 -0400

    add jumpserver file

---
+++ jumpserver/apps/jumpserver/context_processor.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+
+def jumpserver_processor(request):
+    # Setting default pk
+    context = {
+        'DEFAULT_PK': '00000000-0000-0000-0000-000000000000',
+        'LOGO_URL': static('img/logo.png'),
+        'LOGO_TEXT_URL': static('img/logo_text.png'),
+        'LOGIN_IMAGE_URL': static('img/login_image.png'),
+        'FAVICON_URL': static('img/facio.ico'),
+        'JMS_TITLE': 'Jumpserver',
+        'VERSION': settings.VERSION,
+        'COPYRIGHT': _('Beijing Duizhan Tech, Inc.') + ' © 2014-2019'
+    }
+    return context
+
+
+
